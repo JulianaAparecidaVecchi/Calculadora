7f5e1ae9db4f566f73e51ffe8d9f2bd4d28fd135
JulianaAparecidaVecchi <163355132+JulianaAparecidaVecchi@users.noreply.github.com>
2024-05-02 12:04:43 -0300

    Adição de Funções
    Adição de Funções

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,26 @@
 
-print("-----| MENU CALCULADORA |-----")
-print(f"1 - soma;\n2-  subtração;\n3 - multiplicação;\n4 - divisão;\n0 - sair")
+def soma(a,b):
+    soma_resultado= a + b
+    return soma_resultado
+
+def subtracao(a,b):
+    sub_resultado= a - b 
+    return sub_resultado
+
+def multiplicacao(a,b):
+    mul_resultado= a * b
+    return mul_resultado
+
+def divisao(a,b):
+    div_resultado= a / b 
+    return div_resultado
+
+
 while True:
+    print("-----| MENU CALCULADORA |-----")
+    print(f"1 - soma;\n2-  subtração;\n3 - multiplicação;\n4 - divisão;\n0 - sair")
+    print(30*'-')
+    print
     operacao=int(input("QUAL OPERAÇÃO DESEJA FAZER? "))
     if operacao==0:
         print("FECHANDO CALCULADORA...")
@@ -12,16 +31,12 @@ while True:
     num1=float(input("DIGITE O PRIMEIRO NÚMERO: "))
     num2=float(input("DIGITE O SEGUNDO NÚMERO: "))
     if operacao==1:
-        soma=num1+num2
-        print(f"O resultado da sua soma é: {soma:.2f} \nOperação realizada: {num1} + {num2} = {soma:.2f}")
+        print(f"O resultado da sua soma é: {soma(num1,num2):.2f} \nOperação realizada: {num1} + {num2} = {soma(num1,num2):.2f}")
     elif operacao==2: 
-        subtracao = num1-num2
-        print(f"O resultado da sua subtração é: {subtracao:.2f}\nOperação realizada: {num1} - {num2} = {subtracao:.2f}")
+        print(f"O resultado da sua subtração é: {subtracao(num1,num2):.2f}\nOperação realizada: {num1} - {num2} = {subtracao(num1,num2):.2f}")
     elif operacao==3: 
-        multiplicacao = num1*num2
-        print(f"O resultado da sua multiplicação é: {multiplicacao:.2f}\nOperação realizada: {num1} * {num2} = {multiplicacao:.2f}")   
+        print(f"O resultado da sua multiplicação é: {multiplicacao(num1,num2):.2f}\nOperação realizada: {num1} * {num2} = {multiplicacao(num1,num2):.2f}")   
     else : 
-        divisao = num1/num2
-        print(f"O resultado da sua divisão é: {divisao:.2f}\nOperação realizada: {num1} / {num2} = {divisao:.2f}")
+        print(f"O resultado da sua divisão é: {divisao(num1,num2):.2f}\nOperação realizada: {num1} / {num2} = {divisao(num1,num2):.2f}")
  
-print("--------------------------------")    
+print(30*'-')

3646daedbf2dfa4c74a63e65b57265566af2ab8c
JulianaAparecidaVecchi <163355132+JulianaAparecidaVecchi@users.noreply.github.com>
2024-05-10 21:39:56 -0300

    Add Funçoes
    Todas as funcionalidades foram substituídas por funções.

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-
+import time
 def soma(a,b):
     soma_resultado= a + b
     return soma_resultado
@@ -15,28 +15,66 @@ def divisao(a,b):
     div_resultado= a / b 
     return div_resultado
 
-
-while True:
+def exibir_menu():
     print("-----| MENU CALCULADORA |-----")
     print(f"1 - soma;\n2-  subtração;\n3 - multiplicação;\n4 - divisão;\n0 - sair")
     print(30*'-')
-    print
+
+def entrada_operacao():
     operacao=int(input("QUAL OPERAÇÃO DESEJA FAZER? "))
-    if operacao==0:
-        print("FECHANDO CALCULADORA...")
-        break
-    if operacao>4 or operacao<0:
-        print("Opção inválida! Por favor, escolha uma operação da tabela.")
-        continue 
-    num1=float(input("DIGITE O PRIMEIRO NÚMERO: "))
-    num2=float(input("DIGITE O SEGUNDO NÚMERO: "))
-    if operacao==1:
-        print(f"O resultado da sua soma é: {soma(num1,num2):.2f} \nOperação realizada: {num1} + {num2} = {soma(num1,num2):.2f}")
-    elif operacao==2: 
-        print(f"O resultado da sua subtração é: {subtracao(num1,num2):.2f}\nOperação realizada: {num1} - {num2} = {subtracao(num1,num2):.2f}")
-    elif operacao==3: 
-        print(f"O resultado da sua multiplicação é: {multiplicacao(num1,num2):.2f}\nOperação realizada: {num1} * {num2} = {multiplicacao(num1,num2):.2f}")   
-    else : 
-        print(f"O resultado da sua divisão é: {divisao(num1,num2):.2f}\nOperação realizada: {num1} / {num2} = {divisao(num1,num2):.2f}")
- 
+    return operacao
+
+def menu(operacao,num1,num2):
+
+        if operacao==1:
+            linha()
+            print(f"O resultado da sua soma é: {soma(num1,num2):.2f} \nOperação realizada: {num1} + {num2} = {soma(num1,num2):.2f}")
+            linha()
+
+        elif operacao==2: 
+            linha()
+            print(f"O resultado da sua subtração é: {subtracao(num1,num2):.2f}\nOperação realizada: {num1} - {num2} = {subtracao(num1,num2):.2f}")
+            linha()
+        elif operacao==3: 
+            linha()
+            print(f"O resultado da sua multiplicação é: {multiplicacao(num1,num2):.2f}\nOperação realizada: {num1} * {num2} = {multiplicacao(num1,num2):.2f}")  
+            linha()
+        else : 
+            while num2==0:
+                print('É impossivel dividir por zero, digite os números novamente!')
+                num1=float(input("DIGITE O PRIMEIRO NÚMERO: "))
+                num2=float(input("DIGITE O SEGUNDO NÚMERO: "))
+                linha()
+                print(f"O resultado da sua divisão é: {divisao(num1,num2):.2f}\nOperação realizada: {num1} / {num2} = {divisao(num1,num2):.2f}")  
+                linha()   
+
+def pedir_numero1():
+    a=float(input("DIGITE O PRIMEIRO NÚMERO: "))
+    return a
+
+def pedir_numero2(): 
+    b=float(input("DIGITE O SEGUNDO NÚMERO: "))
+    return b
+
+def pausa(x):
+    time.sleep(x)
+
+def linha():
+    print(60*'-')
+
+
+while True:
+        pausa(1)
+        exibir_menu()
+        operacao=entrada_operacao()
+        if operacao==0:
+            print("FECHANDO CALCULADORA...")
+            pausa(1)
+            break
+        if operacao>4 or operacao<0:
+            print("Opção inválida! Por favor, escolha uma operação da tabela.")
+            continue
+        num1=pedir_numero1()
+        num2=pedir_numero2()
+        menu(operacao,num1,num2)
 print(30*'-')
